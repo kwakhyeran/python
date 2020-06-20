e6ecaade9d5b52245093299811f4037026dcbb63
kwakhyeran <kwakli@naver.com>
2020-06-20 17:07:24 +0900

    20200620 project

--- project/mysite/paprikas/urls.py
+++ project/mysite/paprikas/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.main, name="main"),
     path('create/',views.create,name="create"),
     path('index/',views.index,name ="index"),
+    path('delete/<int:paprikas_pk>/',views.delete,name="delete"),
+    path('update/<int:paprikas_pk>/',views.update,name="update"),
+    path('detail/<int:paprikas_pk>/',views.detail,name="detail"),
 
 ]
--- project/mysite/paprikas/views.py
+++ project/mysite/paprikas/views.py
@@ -9,7 +9,7 @@ def main(request):
 def create(request):
     if request.method == "POST":
         form = ArticleForm(request.POST)
-        if form.is_vaild():
+        if form.is_valid():
             article = form.save()
             return redirect('paprikas:index')
     else :
@@ -18,5 +18,41 @@ def create(request):
         'form' : form
     }
     return render(request,'paprikas/create.html',context)
+
+def update(request,paprikas_pk):
+    article = Article.objects.get(pk=paprikas_pk)
+    #post 방식으로 받을 때  - 수정 
+    if request.method == "POST":
+        form = ArticleForm(request.POST,instance=article)
+        if form.is_valid():
+            article = form.save()
+            return redirect('paprikas:detail',article.pk)
+    else :
+        form = ArticleForm(instance=article)
+    context ={
+        'form' : form
+    }
+    return render(request,'paprikas/create.html',context)
+
+def index(request):
+    articles = Article.objects.all()
+    context ={
+        'articles' : articles
+    }
+    return render(request, 'paprikas/index.html', context)
+
+def delete(request,paprikas_pk):
+    article = Article.objects.get(pk = paprikas_pk)
+    if request.method=="POST":
+        article.delete()
+        return redirect('paprikas:index')
+    return redirect('paprikas:detail',article.pk)
+
+def detail(request,paprikas_pk):
+    article = Article.objects.get(pk=paprikas_pk)
+    context = {
+        'article' : article
+    }
+    return render(request,'paprikas/detail.html',context)
     
         
